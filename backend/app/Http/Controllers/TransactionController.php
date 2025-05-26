0dfbfeabfff6d465c685d365490aaa5294bb3c32
Marek K <marek.krama@gmail.com>
2025-05-26 12:19:26 +0200

    test 1

--- backend/app/Http/Controllers/TransactionController.php
+++ backend/app/Http/Controllers/TransactionController.php
@@ -18,56 +18,56 @@ class TransactionController extends Controller
   {
     $transactionsQuery = auth()->user()->transactions()->with(['category', 'type']);
 
-    if ($request->has('category_id')) {
-      $transactionsQuery->where('category_id', $request->input('category_id'));
-    }
+    // if ($request->has('category_id')) {
+    //   $transactionsQuery->where('category_id', $request->input('category_id'));
+    // }
 
-    if ($request->has('start_date') && $request->has('end_date')) {
-      $startDate = Carbon::parse($request->input('start_date'))->timezone('Europe/Warsaw')->startOfDay();
-      $endDate = Carbon::parse($request->input('end_date'))->timezone('Europe/Warsaw')->endOfDay();
+    // if ($request->has('start_date') && $request->has('end_date')) {
+    //   $startDate = Carbon::parse($request->input('start_date'))->timezone('Europe/Warsaw')->startOfDay();
+    //   $endDate = Carbon::parse($request->input('end_date'))->timezone('Europe/Warsaw')->endOfDay();
 
-      $transactionsQuery->whereBetween('date', [$startDate, $endDate]);
-    } elseif ($request->has('start_date')) {
-      $startDate = Carbon::parse($request->input('start_date'))->startOfDay();
+    //   $transactionsQuery->whereBetween('date', [$startDate, $endDate]);
+    // } elseif ($request->has('start_date')) {
+    //   $startDate = Carbon::parse($request->input('start_date'))->startOfDay();
 
-      $transactionsQuery->where('date', '>=', $startDate);
-    } elseif ($request->has('end_date')) {
-      $endDate = Carbon::parse($request->input('end_date'))->timezone('Europe/Warsaw')->endOfDay();
+    //   $transactionsQuery->where('date', '>=', $startDate);
+    // } elseif ($request->has('end_date')) {
+    //   $endDate = Carbon::parse($request->input('end_date'))->timezone('Europe/Warsaw')->endOfDay();
 
-      $transactionsQuery->where('date', '<=', $endDate);
-    }
+    //   $transactionsQuery->where('date', '<=', $endDate);
+    // }
 
-    if ($request->has('sort_by') && in_array($request->input('sort_by'), ['asc', 'desc'])) {
-      $transactionsQuery->orderBy('amount', $request->input('sort_by'));
-    } else if ($request->has('sort_by') && $request->input('sort_by') == 'asc') {
-      $transactionsQuery->orderBy('amount', 'asc');
-    }
+    // if ($request->has('sort_by') && in_array($request->input('sort_by'), ['asc', 'desc'])) {
+    //   $transactionsQuery->orderBy('amount', $request->input('sort_by'));
+    // } else if ($request->has('sort_by') && $request->input('sort_by') == 'asc') {
+    //   $transactionsQuery->orderBy('amount', 'asc');
+    // }
 
-    if ($request->has('search') && $request->input('search') != '') {
-      $transactionsQuery->where('name', 'like', '%' . $request->input('search') . '%');
-    }
+    // if ($request->has('search') && $request->input('search') != '') {
+    //   $transactionsQuery->where('name', 'like', '%' . $request->input('search') . '%');
+    // }
 
-    if ($request->has('type_id')) {
-      $transactionsQuery->where('type_id', $request->input('type_id'));
-    }
+    // if ($request->has('type_id')) {
+    //   $transactionsQuery->where('type_id', $request->input('type_id'));
+    // }
 
-    $income = (float) auth()->user()->transactions()
-      ->whereHas('type', fn($q) => $q
-        ->where('name', 'income'))
-      ->sum('amount');
+    // $income = (float) auth()->user()->transactions()
+    //   ->whereHas('type', fn($q) => $q
+    //     ->where('name', 'income'))
+    //   ->sum('amount');
 
-    $expense = (float) auth()->user()->transactions()
-      ->whereHas('type', fn($q) => $q
-        ->where('name', 'expense'))
-      ->sum('amount');
+    // $expense = (float) auth()->user()->transactions()
+    //   ->whereHas('type', fn($q) => $q
+    //     ->where('name', 'expense'))
+    //   ->sum('amount');
 
     $transactions = $transactionsQuery->paginate($request->input('per_page', 10));
 
     return new TransactionCollection($transactions)->additional([
       "data" => [
-        "income_sum" => round($income, 2),
-        "expense_sum" => round($expense, 2),
-        "balance" => round($income - $expense, 2),
+        // "income_sum" => round($income, 2),
+        // "expense_sum" => round($expense, 2),
+        // "balance" => round($income - $expense, 2),
       ],
     ]);
   }
